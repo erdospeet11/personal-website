71a45952140adf07a5608af45bdb37a90b4df9c5
Peter Erdos <erdospepe9@gmail.com>
2026-02-09 23:16:12 +0100

    feat: add center div

--- personal/src/routes/+page.svelte
+++ personal/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<div id="center-container">
+    Hello
+    <div>
+        Projects
+    </div>
+    
+    <div>
+        Contact
+    </div>
+</div>
+
+
+<style>
+    #center-container{
+        background-color: rgb(143, 146, 146);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        width: 200px;
+        border: 3px solid green; 
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
